a78d8e4d6cb64f4e645652fcc25801572070502a
iasakura <i.asakura58@gmail.com>
2024-09-30 01:20:08 +0900

    Revert "Revert "add test2"" (#6)
    * Revert "Revert "add test2 (#4)" (#5)"
    
    This reverts commit 8735dc4c539c053b73c186e18e722acc74b239a8.
    
    * fix: tests

--- src/integrate_test.erl
+++ src/integrate_test.erl
@@ -10,12 +10,18 @@ setup() ->
 cleanup(_) -> ok.
 
 transition_test_() ->
-    [{setup, fun setup/0, fun cleanup/1, [fun integrate_test_case1/0, fun integrate_test_case2/0]}].
+    [
+        {setup, fun setup/0, fun cleanup/1, [
+            fun integrate_test_case1/0,
+            fun integrate_test_case2/0,
+            fun integrate_test_case3/0
+        ]}
+    ].
 
 integrate_test_case1() ->
     Doc = doc:new(),
     Txn = transaction:new(Doc),
-    {Update, Rest} = update:decode_update(
+    {Update, <<"">>} = update:decode_update(
         <<1, 1, 176, 249, 159, 198, 7, 0, 40, 1, 0, 4, 107, 101, 121, 66, 1, 119, 6, 118, 97, 108,
             117, 101, 66, 0>>
     ),
@@ -27,7 +33,19 @@ integrate_test_case2() ->
     {ok, BinaryContent} = file:read_file("tests/test1.bin"),
     Doc = doc:new(),
     Txn = transaction:new(Doc),
-    {Update, Rest} = update:decode_update(BinaryContent),
+    {Update, <<"">>} = update:decode_update(BinaryContent),
     transaction:apply_update(Txn, Update),
+    ?LOG_DEBUG("store: ~p", [block_store:get_all(Doc#doc.store#store.blocks)]),
+    ok.
+
+integrate_test_case3() ->
+    {ok, A} = file:read_file("tests/test2-a.bin"),
+    {ok, B} = file:read_file("tests/test2-b.bin"),
+    {UpdateA, <<"">>} = update:decode_update(A),
+    {UpdateB, <<"">>} = update:decode_update(B),
+    Doc = doc:new(),
+    Txn = transaction:new(Doc),
+    transaction:apply_update(Txn, UpdateA),
+    transaction:apply_update(Txn, UpdateB),
     ?LOG_DEBUG("store: ~p", [block_store:get_all(Doc#doc.store#store.blocks)]),
     ok.
