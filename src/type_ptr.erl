84fc2b04da56283dd1d651412fd64970033025c7
iasakura <i.asakura58@gmail.com>
2024-09-30 01:17:26 +0900

    Revert "add test2 (#4)"
    This reverts commit cb2eed22abb65d7b3b84e432ac82cf12c41a5778.

--- src/type_ptr.erl
+++ src/type_ptr.erl
@@ -3,7 +3,4 @@
 -export_type([type_ptr/0]).
 
 -type type_ptr() ::
-    {unknown}
-    % | {branch, branch:branch()} % branchに書き換えがあったときに検出できないため削除
-    | {named, binary()}
-    | {id, id:id()}.
+    {unknown} | {branch, branch:branch()} | {named, binary()} | {id, id:id()}.
--- src/types.erl
+++ src/types.erl
@@ -2,28 +2,30 @@
 
 -export([
     new/0,
-    put/2,
+    put/3,
     get/2
 ]).
--export_type([types/0, branch/0]).
+-export_type([types/0, branch_ptr/0, type_ptr/0]).
 
 -include("../include/branch.hrl").
 
--type branch() :: #branch{}.
+-type branch_ptr() :: #branch{}.
 
--opaque types() :: ets:table().
+-type type_ptr() :: {unknown} | {branch, branch_ptr()} | {named, string()} | {id, id:id()}.
 
--record(types_entry, {name :: binary()}).
+-type types() :: ets:table().
+
+-record(types_entry, {name :: binary(), type :: branch_ptr()}).
 
 -spec new() -> types().
-new() -> ets:new(types, [public, set, {keypos, #types_entry.name}]).
+new() -> ets:new(types, [set, {keypos, #types_entry.name}]).
 
--spec put(types(), binary()) -> true.
-put(Types, Name) -> ets:insert(Types, #types_entry{name = Name}).
+-spec put(types(), binary(), branch_ptr()) -> true.
+put(Types, Name, Type) -> ets:insert(Types, #types_entry{name = Name, type = Type}).
 
--spec get(types(), binary()) -> option:option(binary()).
+-spec get(types(), binary()) -> option:option(branch_ptr()).
 get(Types, Name) ->
     case ets:lookup(Types, Name) of
         [] -> undefined;
-        [#types_entry{name = Name}] -> {ok, Name}
+        [{_, Entry}] -> {ok, Entry#types_entry.type}
     end.
