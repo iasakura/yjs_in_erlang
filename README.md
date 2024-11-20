8da4cf7e34c86b65e01a95e3fb5a65fe3a15770a
Izumi Asakura <5370686+iasakura@users.noreply.github.com>
2024-11-21 01:30:46 +0900

    demo

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-yjs_in_erlang
-=====
+# yjs_in_erlang
 
-A pure Erlang implementation of Yjs/Yrs, created for learning. Not intended for production use.
+This is a pure Erlang implementation of Yjs/Yrs, created as a learning project. It includes a simple demo to showcase basic functionality.  
+**Note:** This library is not intended for production use.
 
-Build
------
+## Demo
 
-    $ rebar3 compile
+### Backend
+Start the backend with the following command:
+
+rebar3 shell
+
+### Frontend
+Set up and run the frontend demo:
+
+```
+cd quill-demo
+npm install
+npm start
+```
+
+Once everything is running, you can view the demo at [http://localhost:8080/quill](http://localhost:8080/quill).
+
+---
+
+## Credits
+
+The `quill-demo` directory contains files sourced from [yjs-demos/quill](https://github.com/yjs/yjs-demos/tree/main/quill).
---
+++ quill-demo/quill.js
@@ -0,0 +1,78 @@
+/* eslint-env browser */
+
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
+import { QuillBinding } from "y-quill";
+import Quill from "quill";
+import QuillCursors from "quill-cursors";
+
+Quill.register("modules/cursors", QuillCursors);
+
+window.addEventListener("load", () => {
+  const ydoc = new Y.Doc();
+  const provider = new WebsocketProvider(
+    "ws://localhost:3000/ws", // use the public ws server
+    // `ws${location.protocol.slice(4)}//${location.host}/ws`, // alternatively: use the local ws server (run `npm start` in root directory)
+    "quill-demo-2024/06",
+    ydoc
+  );
+  const ytext = ydoc.getText("quill");
+  const editorContainer = document.createElement("div");
+  editorContainer.setAttribute("id", "editor");
+  document.body.insertBefore(editorContainer, null);
+  editorContainer.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      console.log("document", ydoc);
+    }
+  });
+
+  const editor = new Quill(editorContainer, {
+    modules: {
+      cursors: true,
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ["bold", "italic", "underline"],
+        ["image", "code-block"],
+      ],
+      history: {
+        userOnly: true,
+      },
+    },
+    placeholder: "Start collaborating...",
+    theme: "snow", // or 'bubble'
+  });
+
+  const binding = new QuillBinding(ytext, editor, provider.awareness);
+  ydoc.on("update", (update) => {
+    console.log(ydoc);
+  });
+  ydoc.on("beforeTransaction", (transaction, remote) => {
+    console.log("beforeTransaction", transaction, remote);
+  });
+  ydoc.on("afterTransaction", (transaction, remote) => {
+    console.log("afterTransaction", transaction, remote);
+  });
+
+  /*
+  // Define user name and user name
+  // Check the quill-cursors package on how to change the way cursors are rendered
+  provider.awareness.setLocalStateField('user', {
+    name: 'Typing Jimmy',
+    color: 'blue'
+  })
+  */
+
+  const connectBtn = document.getElementById("y-connect-btn");
+  connectBtn.addEventListener("click", () => {
+    if (provider.shouldConnect) {
+      provider.disconnect();
+      connectBtn.textContent = "Connect";
+    } else {
+      provider.connect();
+      connectBtn.textContent = "Disconnect";
+    }
+  });
+
+  // @ts-ignore
+  window.example = { provider, ydoc, ytext, binding, Y };
+});
